917b5c610f6f00497707a6885bd6f0fe5f0d011b
jun-harashima <j.harashima@gmail.com>
2023-09-08 21:56:37 +0900

    update init.el

--- init.el
+++ init.el
@@ -68,14 +68,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (manoj-dark))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-(put 'set-goal-column 'disabled nil)
-
-;; マジックコメントの自動挿入を無効化
-(setq ruby-insert-encoding-magic-comment nil)
+ '(comint-highlight-prompt ((t (:inherit minibuffer-prompt :foreground "cyan")))))
